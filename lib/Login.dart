bcd81bf5aa867f3df5ab68672eaff14ea9bc4d58
mareenav <mvathalloor@yahoo.com>
2019-02-07 07:09:00 +0100

    login screen

--- lib/Login.dart
+++ lib/Login.dart
@@ -6,7 +6,34 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context){
+  return Container(
+ height: double.infinity,
+  width: double.infinity,
+  color: Colors.white,
+  child: Center(child:new Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Image.asset("Assets/logo.png",height: 100.0,width: 100.0,),
+     new Padding(padding: EdgeInsets.only(top:50.0),child:  RaisedButton(
+         color: Colors.orangeAccent,
+         shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)) ,
+         child: Text(""
+             "Sign in with Amal Jyothi Mail",
+           style: TextStyle(
+               fontSize: 20.0,
+               color: Colors.red
+           ),
+         ),
+         onPressed: (){
+           //action
+         }),)
+    ],
+  )
+
+
+
+  ),
+  );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,40 +8,50 @@ void main()
     debugShowCheckedModeBanner: false,
   ));
 }
-class Splash  extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
   Duration duration=const Duration(seconds: 5);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(duration,(){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>new Login()));
+    }
+    );
+  }
   @override
   Widget build(BuildContext context) {
 
-    Timer(duration,(){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>new Login()));
-    }
-    );
     var height=MediaQuery.of(context).size.height;
     var width=MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
-        height: height,
-        width: width,
-        color: Color(0xffffa200),
-        child:new Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset("Assets/logo.png",height: 100,width: 100,),
-              Padding(
-                padding: EdgeInsets.only(top: 50.0),
-                  child:Text("LocateCab AJCE",style:
+          height: height,
+          width: width,
+          color: Color(0xffffa200),
+          child:new Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("Assets/logo.png",height: 100,width: 100,),
+                Padding(
+                    padding: EdgeInsets.only(top: 50.0),
+                    child:Text("LocateCab AJCE",style:
                     TextStyle(
-                      color: Color(0xfffd4a44),
+                        color: Color(0xfffd4a44),
                         fontSize: 22.0,
-                      fontFamily: 'Gothic',
-                      fontWeight: FontWeight.bold
+                        fontFamily: 'Gothic',
+                        fontWeight: FontWeight.bold
                     ),))
-            ],
-          ),
-        )
+              ],
+            ),
+          )
       ),
-    );
-  }
+    );}
 }
