5fff78666c71e9142ae9ef3d2c9bc8f82d98d9fd
sebyjt <sjkappan@gmail.com>
2019-02-12 21:13:43 +0530

    drawer

--- lib/Landing.dart
+++ lib/Landing.dart
@@ -36,6 +36,70 @@ class _LandingState extends State<Landing> {
 class Drawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: MediaQuery.of(context).size.width*.6,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Stack(children: [new Image.asset("Assets/myava.jpg"),
+          Positioned( bottom: 5.0,
+                      child: Container(
+                      
+                        width: MediaQuery.of(context).size.width*.6,
+                        alignment: Alignment.bottomCenter,child: 
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Mareena Vathaloor",style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  background: Paint()..color=Colors.black.withAlpha(7777)
+                ),),
+                Text("mava@cs.ajce.in",style: TextStyle(
+                  color: Colors.white,
+                   background: Paint()..color=Colors.black.withAlpha(7777),
+                  fontSize: 20.0)) 
+              ],
+            ),),
+          )]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+           
+            Image.asset("Assets/up.png",height: 30.0,width: 50.0,color: Colors.black,),
+            Text("Receiver")
+          ],),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+           
+            Image.asset("Assets/down.png",height: 30.0,width: 50.0,color: Colors.black,),
+            Text("Receiver")
+          ],),
+          new Divider(),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+           
+            Icon(Icons.settings),
+            Text("Settings")
+          ],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+           
+            Icon(Icons.power_settings_new),
+            Text("Receiver")
+          ],),
+          new Image.asset("Assets/ajce.png",height: 80.0,width: 80.0,)
+        ],
+      ),
+    );
   }
 }
