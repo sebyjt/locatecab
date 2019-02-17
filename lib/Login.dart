5503b378020561351b66fc28a9cc4fc43b05494a
Jithin K Jose <jithin.jude68@gmail.com>
2019-02-17 10:27:13 +0530

    The way of displaying splash_screen changed to more efficient.

--- lib/Login.dart
+++ lib/Login.dart
@@ -8,39 +8,41 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context){
-  return Container(
- height: double.infinity,
-  width: double.infinity,
-  color: Colors.white,
-  child: Center(child:new Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Image.asset("Assets/logo.png",height: 100.0,width: 100.0,),
-     new Padding(padding: EdgeInsets.only(top:50.0,left: 20.0,right: 20.0),child:  RaisedButton(
-         color: Colors.orangeAccent,
-         shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)) ,
-         child: new Row(
-           crossAxisAlignment: CrossAxisAlignment.center,
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children:[
-           new Image.asset("Assets/g.png",height:50.0,width:50.0,),
-           Text(""
-             "Sign in with Amal Jyothi Mail",
-           style: TextStyle(
-               fontSize:15.0,
-               color: Colors.white
-           ))],
-         ),
-         onPressed: (){
-           //action
-           Navigator.push(context, MaterialPageRoute(builder: (context)=>new Landing()));
-         }),)
-    ],
-  )
+  return Scaffold(
+    body: Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Colors.white,
+      child: Center(child:new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset("Assets/logo.png",height: 100.0,width: 100.0,),
+          new Padding(padding: EdgeInsets.only(top:50.0,left: 20.0,right: 20.0),child:  RaisedButton(
+              color: Colors.orangeAccent,
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)) ,
+              child: new Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:[
+                  new Image.asset("Assets/g.png",height:50.0,width:50.0,),
+                  Text(""
+                      "Sign in with Amal Jyothi Mail",
+                      style: TextStyle(
+                          fontSize:15.0,
+                          color: Colors.white
+                      ))],
+              ),
+              onPressed: (){
+                //action
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>new Landing()));
+              }),)
+        ],
+      )
 
 
 
-  ),
+      ),
+    ),
   );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,54 +4,7 @@ import 'Login.dart';
 void main()
 {
   runApp(MaterialApp(
-    home: Splash(),
+    home: Login(),
     debugShowCheckedModeBanner: false,
   ));
-}
-class Splash extends StatefulWidget {
-  @override
-  _SplashState createState() => _SplashState();
-}
-
-class _SplashState extends State<Splash> {
-  Duration duration=const Duration(seconds: 5);
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    Timer(duration,(){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>new Login()));
-    }
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-
-    var height=MediaQuery.of(context).size.height;
-    var width=MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Container(
-          height: height,
-          width: width,
-          color: Colors.white,
-          child:new Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset("Assets/logo.png",height: 100,width: 100,),
-                Padding(
-                    padding: EdgeInsets.only(top: 50.0),
-                    child:Text("LocateCab AJCE",style:
-                    TextStyle(
-                        color: Color(0xfffd4a44),
-                        fontSize: 22.0,
-                        fontFamily: 'Gothic',
-                        fontWeight: FontWeight.bold
-                    ),))
-              ],
-            ),
-          )
-      ),
-    );}
 }
