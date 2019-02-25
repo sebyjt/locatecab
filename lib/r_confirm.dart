751d0df9de49ec2096d135c48b2529a5327a8a67
Suhail Haris <sh.skyinfo@gmail.com>
2019-02-25 21:38:27 +0530

    edit reciever confirmation page

--- lib/r_confirm.dart
+++ lib/r_confirm.dart
@@ -29,11 +29,33 @@ class _ConfirmRState extends State<ConfirmR> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+              padding: const EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
               child: new Text(
                 "Your Location is live on the Map\n"
-                    "Please wait until a host accepts you",
+                    "Please wait until a host accepts you\n",
                 textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+              child: new Image.asset('Assets/movingcar.gif',
+                  alignment: Alignment.center),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 0.0),
+              child: Container(
+                width: 250.0,
+                height: 45.0,
+                child: new RaisedButton(
+                  splashColor: Colors.red.withAlpha(700),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(70.0)),
+                  color: Colors.orangeAccent.withAlpha(700),
+                  child: Text(
+                    "Done",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
             )
           ],
