e2435c16475d309cf3b1f6bc82677e4628c4c40c
mareenav <mvathalloor@yahoo.com>
2019-02-28 11:29:55 +0530

    first login and host linking

--- lib/Firstlogin.dart
+++ lib/Firstlogin.dart
@@ -9,22 +9,47 @@ class _FirstloginState extends State<Firstlogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: true,
         body: Container(
             height: double.infinity,
             width: double.infinity,
             color: Colors.white,
+            padding: EdgeInsets.only(top: 30.0),
             child: Center(
-                child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                child: new ListView(
                     children: <Widget>[
                   Image.asset(
                     "Assets/logo.png",
                     height: 100.0,
                     width: 100.0,
                   ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40.0,right: 40.0,top: 20.0),
+                        child: TextField(
+                            keyboardType:  TextInputType.number,
+                            decoration:new InputDecoration(labelText: "contact no")),
+                      ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40.0,right: 40.0,top: 20.0),
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: new InputDecoration(labelText: "Capacity"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.only(right: 40.0,left: 40.0,top:20.0),
+                    child: TextField(
+
+                      decoration: new InputDecoration(labelText: "Car Model"),
+                    ),
+                  ),
                   new Padding(
                     padding:
                         EdgeInsets.only(top: 60.0, left: 40.0, right: 20.0),
+
                     child: RaisedButton(
                       onPressed: (){},
                       color: Colors.orangeAccent,
