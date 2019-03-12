10882110b75bd5656f2ec58da768231e692e9ea6
Suhail Haris <sh.skyinfo@gmail.com>
2019-03-12 10:14:15 +0530

    add hosts, user in db
    hosts - car name, capacity
    user - name, contact no

--- lib/crud.dart
+++ lib/crud.dart
@@ -2,7 +2,11 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CrudMethods {
-   Future<void> addData(UserData) async {
+   Future<void> addDataU(UserData) async {
     Firestore.instance.collection('users').add(UserData);
   }
+
+   Future<void> addDataH(UserData) async {
+     Firestore.instance.collection('hosts').add(UserData);
+   }
 }
